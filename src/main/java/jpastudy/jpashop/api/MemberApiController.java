a3abdba5fb1fc356ec45e8614c9b0749c37b86b9
mysoyul <vega2k_@naver.com>
2023-05-23 14:18:55 +0900

    update method add

--- src/main/java/jpastudy/jpashop/api/MemberApiController.java
+++ src/main/java/jpastudy/jpashop/api/MemberApiController.java
@@ -2,11 +2,10 @@ package jpastudy.jpashop.api;
 
 import jpastudy.jpashop.domain.Member;
 import jpastudy.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
@@ -33,6 +32,28 @@ public class MemberApiController {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
+
+    /**
+     * 수정 API
+     */
+    @PatchMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
     @Data
     static class CreateMemberRequest {
         @NotBlank(message = "Name은 필수 입력항목 입니다.")
