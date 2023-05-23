d96a26e9e17642b470b286734bbabf920f2a1881
mysoyul <vega2k_@naver.com>
2023-05-23 16:37:51 +0900

    OrderApiController page request

--- src/main/java/jpastudy/jpashop/repository/OrderRepository.java
+++ src/main/java/jpastudy/jpashop/repository/OrderRepository.java
@@ -41,11 +41,13 @@ public class OrderRepository {
 	//Order => Member, Delivery, OrderItems, Item
 	public List<Order> findAllWithItem() {
 		return em.createQuery(
-						"select  o from Order o" +
+						"select distinct o from Order o" +
 								" join fetch o.member m" +
 								" join fetch o.delivery d" +
 								" join fetch o.orderItems oi" +
 								" join fetch oi.item i", Order.class)
+				.setFirstResult(0)
+				.setMaxResults(100)
 				.getResultList();
 	}
 
