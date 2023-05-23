a25bbfff4a83b412ec0c15642c913fc6d7511285
mysoyul <vega2k_@naver.com>
2023-05-23 15:33:28 +0900

    QueryDto add

--- src/main/java/jpastudy/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpastudy/jpashop/api/OrderSimpleApiController.java
@@ -45,7 +45,8 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v3/simple-orders")
     public List<SimpleOrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithMemberDelivery();
-        List<SimpleOrderDto> result = orders.stream()
+        List<SimpleOrderDto> result = orders
+                .stream()
                 .map(o -> new SimpleOrderDto(o))
                 .collect(Collectors.toList());
         return result;
