d098a1bf36c16af361a9d1fa201907ede6678117
mysoyul <vega2k_@naver.com>
2023-05-23 13:26:49 +0900

    Member Controller level2

--- src/main/java/jpastudy/jpashop/service/MemberService.java
+++ src/main/java/jpastudy/jpashop/service/MemberService.java
@@ -2,6 +2,7 @@ package jpastudy.jpashop.service;
 
 import java.util.List;
 
+import jpastudy.jpashop.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public class MemberService {
 	private void validateDuplicateMember(Member member) {
 		List<Member> findMembers = memberRepository.findByName(member.getName());
 		if (!findMembers.isEmpty()) {
-			throw new IllegalStateException("이미 존재하는 회원입니다.");
+			throw new BusinessException("이미 존재하는 회원입니다.");
 		}
 	}
 
